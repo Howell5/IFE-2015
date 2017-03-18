bebd5bcfde631b6e2b77ffdc77c67fe29ad7b227
howell5 <howell95@outlook.com>
2017-03-18 09:43:06 +0800

    2017/3/18

---
+++ task0003/js/main.js
@@ -0,0 +1,21 @@
+// 避免污染全局变量
+'use strict'
+(function (){
+
+	function main () {
+		// TODO: 执行入口
+	}
+
+	function todo () {
+		// TODO: 任务编辑操作
+	}
+
+	function renderBase () {
+		// TODO: 任务列表渲染
+	}
+
+	function UImodel () {
+		//TODO: 页面交互效果
+	}
+
+})();
